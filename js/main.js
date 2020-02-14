d0001cee9a4514b78a938074046ebf3feb542208
Daniel Mironenko <dm25an22@gmail.com>
2020-02-14 11:25:32 +0100

    Fix

--- js/main.js
+++ js/main.js
@@ -329,29 +329,20 @@ var guests = document.querySelector('#capacity');
 
 var checkSelected = function () {
 
-  switch (true) {
-    case rooms.value === '1' && guests !== '1':
+  if (rooms.value === '1' && guests.value !== '1') {
       rooms.setCustomValidity('В одной комнате может проживать только один гость');
-      break;
-
-    case rooms.value === '2' && (guests !== '1' && guests !== '2'):
-      rooms.setCustomValidity('В двух комнатах могут продивать не более двух гостей');
-      break;
-
-    case rooms.value === '3' && (guests !== '1' && guests !== '2' && guests !== '3'):
+    } else if (rooms.value === '2' && (guests.value !== '1' && guests.value !== '2')) {
+      rooms.setCustomValidity('В двух комнатах могут проживать не более двух гостей');
+    } else if (rooms.value === '3' && (guests.value !== '1' && guests.value !== '2' && guests.value !== '3')) {
       rooms.setCustomValidity('В трёх комнатах могут проживать до трёх человек');
-      break;
-
-    case rooms.value === '100' && guests !== 'не для гостей':
+    } else if (rooms.value === '100' && guests.value !== '0') {
       rooms.setCustomValidity('Не для гостей');
-      break;
-
-    default:
-      rooms.setCustomValidity('');
-      break;
-  }
+    } else {
+      rooms.setCustomValidity("");
+    }
 
 };
+
 
 rooms.addEventListener('change', checkSelected);
 guests.addEventListener('change', checkSelected);
